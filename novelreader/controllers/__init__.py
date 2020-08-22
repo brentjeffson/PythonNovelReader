3d32a5a5bb2be3cbb3c078b6719d98ce87f82a67
Brent Jeffson F. Florendo <brentjeffson@gmail.com>
2020-08-22 21:02:48 +0800

    moved some import statements to package init file

---
+++ novelreader/controllers/__init__.py
@@ -0,0 +1,13 @@
+from kivy.app import Builder
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.recycleview import RecycleView
+
+
+def plog(title, msg=''):
+    title = ''.join([f"[{i.upper()}]" for i in title])
+    print(f'{title} {msg}')
