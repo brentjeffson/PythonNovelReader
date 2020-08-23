15ff474ea4aeb5defe0120d019ae2d2d2d5732b8
Brent Jeffson F. Florendo <brentjeffson@gmail.com>
2020-08-23 19:20:02 +0800

    update plog() to convert msg to str. create dynamic function to load design files. import recycleview dependencies to allow selection of list item.

--- novelreader/controllers/__init__.py
+++ novelreader/controllers/__init__.py
@@ -1,5 +1,5 @@
 from kivy.app import Builder
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty, BooleanProperty
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
@@ -7,8 +7,19 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.recycleview import RecycleView
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 
+from pathlib import Path
 
 def plog(title, msg=''):
     title = ''.join([f"[{i.upper()}]" for i in title])
-    print(f'{title} {msg}')
+    print(f'{title} {str(msg)}')
+
+# load all design files
+# for path in Path('../views').glob('*.kv'):
+#     plog(['KV'], path)
+#     Builder.load_file(str(path))
+
