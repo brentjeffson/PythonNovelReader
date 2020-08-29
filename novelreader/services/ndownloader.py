6256ed26b4fa128f33c3c99d23ce605a31aa1ed3
brentjeffson <brentjeffson@gmail.com>
2020-08-30 07:38:28 +0800

    removed async on functions

--- novelreader/services/ndownloader.py
+++ novelreader/services/ndownloader.py
@@ -8,20 +8,20 @@ def fetch_markup(session: requests.Session, url: str) -> str:
     with session.get(url) as resp:
         return resp.text, resp.status_code
 
-async def parse_markup(markup: str, parser: str = "html.parser"):
+def parse_markup(markup: str, parser: str = "html.parser"):
     """Parse markup using BeautifulSoup"""
     soup = BeautifulSoup(markup, features="html.parser")
     return soup
 
 # parse novel information together with its chapters
-async def get_novel(url: str, soup: BeautifulSoup, parser: NovelBaseParser) -> Novel:
+def get_novel(url: str, soup: BeautifulSoup, parser: NovelBaseParser) -> Novel:
     """Get data on the novel from the parsed markup `soup`"""
     novel = parser.get_novel(soup)
     novel.url = url
     return novel
 
 # todo parse_content, parse content of a chapter
-async def get_content(soup: BeautifulSoup, parser: NovelBaseParser) -> str:
+def get_content(soup: BeautifulSoup, parser: NovelBaseParser) -> str:
     """Get content of chapter from parsed chapter markup."""
     content = parser.get_content(soup)
     return content
