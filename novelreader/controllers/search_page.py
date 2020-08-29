7dbe4002b996a4041e4634a9af898dd3be875e4d
brentjeffson <brentjeffson@gmail.com>
2020-08-29 14:29:44 +0800

    add temporary function to search BoxNovel.

--- novelreader/controllers/search_page.py
+++ novelreader/controllers/search_page.py
@@ -3,17 +3,47 @@ from . import ObjectProperty, StringProperty
 from . import Label, Button
 from . import plog, Path
 
+import aiohttp
+import asyncio
+
 Builder.load_file(str(Path("novelreader/views/search_page.kv").absolute()))
 
 class SearchPage(Screen):
     search_list_recycle = ObjectProperty()
-    search_keyword = StringProperty()
+    search_input = ObjectProperty()
 
     def __init__(self, **kwargs):
         super(SearchPage, self).__init__(**kwargs)
 
-    def search(self, keyword):
-        plog(['searching'], keyword)
+    async def fetch_novels(self, session, payload):
+        async with session.post("https://boxnovel.com/wp-admin/admin-ajax.php", data=payload) as resp:
+            return await resp.json()
+    
+    async def prep_fetch(self):
+        async with aiohttp.ClientSession() as session:
+            payload = {
+                "action": "wp-manga-search-manga",
+                "title": self.search_input.text
+            }
+            self.search_list_recycle.data = []
+            res = await self.fetch_novels(session, payload)
+            print(res['data'])
+            data = res["data"]
+            if data:
+                for item in data:
+                    self.search_list_recycle.data.append({"text": item["title"], "url": item["url"]})
+
+
+    def search(self):
+        plog(['searching'], self.search_input.text)
+        
+        loop = None
+        try:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(self.prep_fetch())
+        except Exception as ex:
+            plog(["search page", 'exception'], ex.with_traceback(None))
+        # 
 
 class SearchItem(Button):
     url = StringProperty()
