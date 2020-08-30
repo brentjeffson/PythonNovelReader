2e6f2b1bd0c9b7a5617f64af47736173a6b7c23a
brentjeffson <brentjeffson@gmail.com>
2020-08-30 13:29:52 +0800

    add update_content(content)

--- novelreader/controllers/reader_page.py
+++ novelreader/controllers/reader_page.py
@@ -6,3 +6,6 @@ Builder.load_file(str(Path('novelreader/views/reader_page.kv').absolute()))
 
 class ReaderPage(Screen):
     """Show chapter content of novel"""
+
+    def update_content(self, content):
+        self.ids.content.text = content
