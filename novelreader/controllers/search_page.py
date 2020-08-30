f6886e56cd107d7f5cb02f370956cfa3eb49fea7
brentjeffson <brentjeffson@gmail.com>
2020-08-30 08:42:47 +0800

    update to use thread, update imports

--- novelreader/controllers/search_page.py
+++ novelreader/controllers/search_page.py
@@ -1,10 +1,17 @@
-from . import Screen, Builder
-from . import ObjectProperty, StringProperty
-from . import Label, Button
-from . import plog, Path
-
-import aiohttp
-import asyncio
+from kivy.app import Builder
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from wescrape.models.novel import Website
+from wescrape.parsers.helpers import identify_parser
+from wescrape.parsers.nparse import BoxNovelCom, WuxiaWorldCo
+from novelreader.services.ndownloader import (fetch_markup, parse_markup, get_novel)
+from . import plog
+from pathlib import Path
+from functools import partial
+import requests
 
 Builder.load_file(str(Path("novelreader/views/search_page.kv").absolute()))
 
@@ -15,46 +22,50 @@ class SearchPage(Screen):
     def __init__(self, **kwargs):
         super(SearchPage, self).__init__(**kwargs)
 
-    def goto_info_page(self, url):
-        self.manager.get_screen("info_page").fetch_novel(url)
-        self.manager.current = "info_page"
+    def goto_info_page(self, url, _):        
+        info_page = self.manager.get_screen("info_page")
+        info_page.ids.chapter_list.data.clear()
 
-    async def fetch_novels(self, session, payload):
-        async with session.post("https://boxnovel.com/wp-admin/admin-ajax.php", data=payload) as resp:
-            return await resp.json()
+        with requests.Session() as session:
+            parser_type = identify_parser(url)
+            parser = None
+            if Website.BOXNOVELCOM == parser_type:
+                parser = BoxNovelCom()
+            elif Website.WUXIAWORLDCO == parser_type:
+                parser = WuxiaWorldCo()
+            markup, status_code = fetch_markup(session, url)
+            soup = parse_markup(markup)
+            novel = get_novel(url, soup, parser)
+            info_page.update_widgets(novel)
+            self.manager.current = "info_page"
+
+    def fetch_novels(self, session, _):
+        payload = {
+            "action": "wp-manga-search-manga",
+            "title": self.search_input.text
+        }
+        resp = session.post("https://boxnovel.com/wp-admin/admin-ajax.php", data=payload)
+        print(resp.json())
+        self.update_search_list(resp.json()["data"])
+
+    def update_search_list(self, novels: {}):
+        for item in novels:
+            self.search_list_recycle.data.append({"text": item["title"], "url": item["url"]})
     
-    async def prep_fetch(self):
-        async with aiohttp.ClientSession() as session:
-            payload = {
-                "action": "wp-manga-search-manga",
-                "title": self.search_input.text
-            }
-            self.search_list_recycle.data = []
-            res = await self.fetch_novels(session, payload)
-            print(res['data'])
-            data = res["data"]
-            if data:
-                for item in data:
-                    self.search_list_recycle.data.append({"text": item["title"], "url": item["url"]})
-
-
     def search(self):
         plog(['searching'], self.search_input.text)
+        self.search_list_recycle.data = []
+        with requests.Session() as session:
+            Clock.schedule_once(partial(self.fetch_novels, session), 0)
         
-        loop = None
-        try:
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(self.prep_fetch())
-        except Exception as ex:
-            plog(["search page", 'exception'], ex.with_traceback(None))
-        # 
 
 class SearchItem(Button):
     url = StringProperty()
 
     def browse(self):
         plog(['browsing'], self.text)
-        self.parent.parent.parent.parent.goto_info_page(self.url)
+        Clock.schedule_once(partial(self.parent.parent.parent.parent.goto_info_page, self.url), 0)
+        
 
 
 
