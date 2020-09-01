a28be6f722b43ce1159e8f3082d26ea692a60432
brentjeffson <brentjeffson@gmail.com>
2020-09-01 12:58:46 +0800

    [U] search function to use threading

--- novelreader/controllers/search_page.py
+++ novelreader/controllers/search_page.py
@@ -5,12 +5,13 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from wescrape.models.novel import Website
-from wescrape.helpers import identify_parser
+from wescrape.helpers import identify_parser, search
 from wescrape.parsers.nparse import BoxNovelCom, WuxiaWorldCo
 from novelreader.helpers import plog
 from pathlib import Path
 from functools import partial
 import requests
+import threading
 
 Builder.load_file(str(Path("novelreader/views/search_page.kv").absolute()))
 
@@ -20,6 +21,7 @@ class SearchPage(Screen):
 
     def __init__(self, **kwargs):
         super(SearchPage, self).__init__(**kwargs)
+        self.__searching = False
 
     def goto_info_page(self, url, _):        
         self.manager.get_screen("info_page").update_widgets(url)
@@ -39,24 +41,36 @@ class SearchPage(Screen):
             except requests.exceptions.ConnectionError as ex:
                 plog(["retry"], i+1)
 
-        
-
     def update_search_list(self, novels: {}):
-        for item in novels:
-            self.search_list_recycle.data.append({"text": item["title"], "url": item["url"]})
-    
-    def search(self):
-        plog(['searching'], self.search_input.text)
-        self.search_list_recycle.data = []
-        with requests.Session() as session:
-            Clock.schedule_once(partial(self.fetch_novels, session), 0)           
+        for novel in novels:
+            self.search_list_recycle.data.append({"text": novel.title, "url": novel.url})
+
+    def search_work(self, session: requests.Session, keyword: str, website: Website):
+        novels = search(session, keyword, website)
+        if novels:
+            plog(["# Of Searched"], len(novels))
+            self.update_search_list(novels)
+        self.__searching = False
+
+    def start_search(self):
+        if not self.__searching:
+            self.__searching = True
+            plog(['searching'], self.search_input.text)
+            self.search_list_recycle.data.clear()
+
+            with requests.Session() as session:
+                thread = threading.Thread(target=self.search_work, args=(session, self.search_input.text, Website.BOXNOVELCOM))
+                thread.start()
+        else:
+            plog(["already searching"], self.search_input.text)
         
 class SearchItem(Button):
     url = StringProperty()
 
     def browse(self):
         plog(['browsing'], self.text)
-        Clock.schedule_once(partial(self.parent.parent.parent.parent.goto_info_page, self.url), 0)
+        self.parent.parent.parent.parent.goto_info_page(self.url)
+        # Clock.schedule_once(partial(), 0)
 
 
 
