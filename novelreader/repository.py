f373334302355bd3a0d040ed744368d6a98b32a3
brentjeffson <brentjeffson@gmail.com>
2020-09-01 13:14:52 +0800

    [U][R] database connection and requests session

--- novelreader/repository.py
+++ novelreader/repository.py
@@ -14,30 +14,17 @@ class Repository:
     INSTANCE = None
 
     @classmethod
-    def build(cls, 
-        db: sql.Connection, 
-        session: requests.Session, 
-        service: Service, 
-        database: Database
-    ) -> Repository:
-
+    def build(cls, service: Service, database: Database) -> Repository:
         if cls.INSTANCE is None:
-            cls.INSTANCE = Repository(db, session, service, database)
+            cls.INSTANCE = Repository(service, database)
         return cls.INSTANCE
 
     @classmethod
     def instance(cls):
         return cls.INSTANCE
 
-    def __init__(self, 
-        db: sql.Connection, 
-        session: requests.Session, 
-        service: Service, 
-        database: Database
-    ):
-
+    def __init__(self, service: Service, database: Database):
         self.__db = db
-        self.__session = session
         self.__service = service
         self.__database = database
     
@@ -46,41 +33,41 @@ class Repository:
         # get from web
         novel = None
         try: 
-            novel = self.__service.fetch_novel(self.__session, url)
+            novel = self.__service.fetch_novel(url)
             if novel is not None:
                 return novel
         except Exception as ex:
             plog(["exception", "get_novel"], ex)
         finally:
             # get from database
-            novel = self.__database.select_novel(self.__db, url)
+            novel = self.__database.select_novel(url)
         return novel
 
     def get_chapters(self, url: str):
         # get from web
         chapters = []
         try: 
-            chapters = self.__service.fetch_chapters(self.__session, url)
+            chapters = self.__service.fetch_chapters(url)
             if chapters is not None:
                 return chapters
         except Exception as ex:
             plog(["exception", "get_chapters"], ex)
         finally:
             # get from database
-            chapters = self.__database.select_chapters(self.__db, url)
+            chapters = self.__database.select_chapters(url)
         return chapters
 
     def get_meta():
         # get from web
         meta = []
         try: 
-            meta = self.__service.fetch_meta(self.__session, url)
+            meta = self.__service.fetch_meta(url)
             if meta is not None:
                 return meta
         except Exception as ex:
             plog(["exception", "get_chapters"], ex)
         finally:
             # get from database
-            meta = self.__database.select_meta(self.__db, url)
+            meta = self.__database.select_meta(url)
         return meta
 
