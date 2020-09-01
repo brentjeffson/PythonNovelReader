99e399702d00f391ec7e17caeb33e2c68c1245db
brentjeffson <brentjeffson@gmail.com>
2020-09-01 15:10:10 +0800

    [A] thumbnail_path()

--- novelreader/helpers.py
+++ novelreader/helpers.py
@@ -1,3 +1,16 @@
+from pathlib import Path
+
 def plog(title, msg=''):
     title = ''.join([f"[{i.upper()}]" for i in title])
     print(f'{title} {str(msg)}')
+
+
+def thumbnail_path(url: str):
+    """Create a path to thumbnail using URL"""
+    thumbnail_path = Path(
+        "novelreader", 
+        "public", 
+        "imgs", 
+        url.split("/")[-1]
+    ).absolute()
+    return thumbnail_path
