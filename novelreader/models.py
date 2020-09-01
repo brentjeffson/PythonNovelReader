0601f34b952df5bb1331e9e2b428c4312309e05f
brentjeffson <brentjeffson@gmail.com>
2020-09-01 14:38:18 +0800

    [U][I] select functions

--- novelreader/models.py
+++ novelreader/models.py
@@ -152,26 +152,78 @@ class Database:
             
         return self.__conn.execute(statement, where_val)
 
-    def select_novels(self):
+    def __convert_row(row: dict, obj_type):
+        converted_row = None
+        if row:
+            try:
+                if obj_type == Novel:
+                    converted_row = Novel(
+                        url=row["url"],
+                        title=row["title"],
+                        thumbnail=row["thumbnail"]
+                    )
+                elif obj_type == Chapter:
+                    converted_row = Chapter(
+                        id=row["chapter_id"],
+                        url=row["url"],
+                        title=row["title"],
+                        content=row["content"],
+                    )
+                elif obj_type == Meta:
+                    converted_row = Meta(
+                        authors=[row["authors"].split(", ")],
+                        genres=[row["genres"].split(", ")],
+                        rating=row["rating"],
+                        release_date=row["release_date"],
+                        status=row["status"],
+                        description=row["description"],
+                    )
+            except Exception as ex:
+                raise ex
+        return converted_row
+
+    def __convert_rows(rows: [dict], convert_type):
+        converted_rows = []
+        if rows:
+            for row in rows:
+                converted_row = self.__convert_row(row, convert_type)
+                if converted_row:
+                    converted_rows.append(converted_row)
+        return converted_rows
+
+    def select_novels(self) -> [Novel]:
         cur = Database.__select(self.__conn, [], "novels")
-        return cur.fetchall()
+        novel_rows = cur.fetchall()
+        novels = self.__convert_rows(novel_rows, Novel)
+        return novels
 
-    def select_novel(self, novel_url):
+
+    def select_novel(self, novel_url) -> Novel:
         cur = Database.__select(self.__conn, [], "novels", ["url"], (novel_url,))
-        return cur.fetchone()
+        novel_row = cur.fetchone()
+        novel = self.__convert_row(novel_row, Novel)
+        return novel
 
-    def select_chapters(self):
+    def select_chapters(self) -> [Chapter]:
         cur = Database.__select(self.__conn, [], "chapters")
-        return cur.fetchall() 
+        chapter_rows = cur.fetchall()
+        chapters = self.__convert_rows(chapter_rows, Chapter)
+        return chapters
 
-    def select_chapter(self, novel_url):
+    def select_chapter(self, novel_url) -> Chapter:
         cur = Database.__select(self.__conn, [], "chapters", ["novel_url"], (novel_url,))
-        return cur.fetchone()
+        chapter_row = cur.fetchone()
+        chapter = self.__convert_row(chapter_row, Chapter)
+        return chapter
 
-    def select_metas(self):
+    def select_metas(self) -> Metas:
         cur = Database.__select(self.__conn, [], "metas")
-        return cur.fetchall()
+        meta_rows = cur.fetchall()
+        metas = self.__convert_rows(meta_rows, Meta)
+        return metas
 
-    def select_meta(self, novel_url):
+    def select_meta(self, novel_url) -> Meta:
         cur = Database.__select(self.__conn, [], "metas", ["novel_url"], (novel_url,))
-        return cur.fetchone()
+        meta_row = cur.fetchone()
+        meta = self.__convert_row(meta_row, Meta)
+        return meta
