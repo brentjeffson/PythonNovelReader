ebab0113fb952d0c83934a0f3bd81cd9cc734dbc
brentjeffson <brentjeffson@gmail.com>
2020-09-07 19:47:42 +0800

    add show() function for decorator

--- novelreader/helpers.py
+++ novelreader/helpers.py
@@ -1,5 +1,16 @@
 from pathlib import Path
 
+def show(funct):
+    def inner(*args, **kwargs):
+        res = funct(*args, **kwargs)
+        print("="*len(str(res)))
+        plog(["args"], args)
+        plog(["kwargs"], args)
+        print(res)
+        print("="*len(str(res)))
+        return res
+    return inner
+
 def plog(title, msg=''):
     title = ''.join([f"[{i.upper()}]" for i in title])
     print(f'{title} {str(msg)}')
